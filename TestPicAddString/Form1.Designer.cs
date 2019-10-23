0b2bcf8b59c9c4067092db3c4984fefba56b6aba
KuaFather <mazhaokai1991@163.com>
2019-10-23 17:12:25 +0800

    第一次提交：该版本还需优化，计算文字背景灰度平均值比较慢，可以考虑对文字背景块先进行采样处理，降低计算量。

---
+++ TestPicAddString/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TestPicAddString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddString = new System.Windows.Forms.Button();
+            this.btnShowOld = new System.Windows.Forms.Button();
+            this.rtbxStringBuf = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxY = new System.Windows.Forms.TextBox();
+            this.tbxX = new System.Windows.Forms.TextBox();
+            this.pbxShowPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxShowPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddString
+            // 
+            this.btnAddString.Location = new System.Drawing.Point(164, 61);
+            this.btnAddString.Name = "btnAddString";
+            this.btnAddString.Size = new System.Drawing.Size(75, 23);
+            this.btnAddString.TabIndex = 1;
+            this.btnAddString.Text = "AddString";
+            this.btnAddString.UseVisualStyleBackColor = true;
+            this.btnAddString.Click += new System.EventHandler(this.btnAddString_Click);
+            // 
+            // btnShowOld
+            // 
+            this.btnShowOld.Location = new System.Drawing.Point(69, 61);
+            this.btnShowOld.Name = "btnShowOld";
+            this.btnShowOld.Size = new System.Drawing.Size(75, 23);
+            this.btnShowOld.TabIndex = 2;
+            this.btnShowOld.Text = "ShowOld";
+            this.btnShowOld.UseVisualStyleBackColor = true;
+            this.btnShowOld.Click += new System.EventHandler(this.btnShowOld_Click);
+            // 
+            // rtbxStringBuf
+            // 
+            this.rtbxStringBuf.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rtbxStringBuf.Location = new System.Drawing.Point(52, 229);
+            this.rtbxStringBuf.Name = "rtbxStringBuf";
+            this.rtbxStringBuf.Size = new System.Drawing.Size(228, 263);
+            this.rtbxStringBuf.TabIndex = 3;
+            this.rtbxStringBuf.Text = "时  间：2019-08-05 10:19:59\n站  点：三河市S213\n车  牌：豫A51258\n轴  数：6\n总  重：241526\n限  重：36000\n" +
+    "超限率：0.0%\n";
+            this.rtbxStringBuf.TextChanged += new System.EventHandler(this.rtbxStringBuf_TextChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.tbxSize);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.tbxY);
+            this.splitContainer1.Panel1.Controls.Add(this.tbxX);
+            this.splitContainer1.Panel1.Controls.Add(this.btnShowOld);
+            this.splitContainer1.Panel1.Controls.Add(this.rtbxStringBuf);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAddString);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pbxShowPic);
+            this.splitContainer1.Size = new System.Drawing.Size(883, 589);
+            this.splitContainer1.SplitterDistance = 294;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SizePerWidth:";
+            // 
+            // tbxSize
+            // 
+            this.tbxSize.Location = new System.Drawing.Point(108, 164);
+            this.tbxSize.Name = "tbxSize";
+            this.tbxSize.Size = new System.Drawing.Size(100, 21);
+            this.tbxSize.TabIndex = 8;
+            this.tbxSize.Text = "0.5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "X:";
+            // 
+            // tbxY
+            // 
+            this.tbxY.Location = new System.Drawing.Point(108, 130);
+            this.tbxY.Name = "tbxY";
+            this.tbxY.Size = new System.Drawing.Size(100, 21);
+            this.tbxY.TabIndex = 5;
+            this.tbxY.Text = "800";
+            // 
+            // tbxX
+            // 
+            this.tbxX.Location = new System.Drawing.Point(108, 103);
+            this.tbxX.Name = "tbxX";
+            this.tbxX.Size = new System.Drawing.Size(100, 21);
+            this.tbxX.TabIndex = 4;
+            this.tbxX.Text = "800";
+            // 
+            // pbxShowPic
+            // 
+            this.pbxShowPic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxShowPic.Location = new System.Drawing.Point(0, 0);
+            this.pbxShowPic.Name = "pbxShowPic";
+            this.pbxShowPic.Size = new System.Drawing.Size(585, 589);
+            this.pbxShowPic.TabIndex = 1;
+            this.pbxShowPic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 589);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxShowPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAddString;
+        private System.Windows.Forms.Button btnShowOld;
+        private System.Windows.Forms.RichTextBox rtbxStringBuf;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pbxShowPic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxY;
+        private System.Windows.Forms.TextBox tbxX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxSize;
+    }
+}
+
---
+++ TestPicAddString/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestPicAddString
+{
+    public partial class Form1 : Form
+
+    {
+        private Image NewPic;
+        Image Oldpic = Image.FromFile("./Images/Old.jpg");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowOld_Click(object sender, EventArgs e)
+        {
+            Oldpic = Image.FromFile("./Images/Old.jpg");
+
+            pbxShowPic.BackgroundImage = Oldpic;
+            pbxShowPic.BackgroundImageLayout = ImageLayout.Stretch;
+
+           
+
+        } 
+        private void btnAddString_Click(object sender, EventArgs e)
+        {
+            colorbuf = 255;
+            cuowei = 0;
+            string[] strbuf = rtbxStringBuf.Lines;
+
+            int MaxLen = 0;//半字的个数
+            foreach (var stringbuf in strbuf)
+            {
+                char[] xx = stringbuf.ToCharArray();
+
+                int lenbuf = xx.Length;
+
+                //判断是否是汉字，汉字显示是字母数字的一半
+                foreach (char wordBuf in xx)
+                {
+                    if (wordBuf > 0xff)
+                    {
+                        lenbuf++;
+                    }
+                }
+                MaxLen = MaxLen > lenbuf ? MaxLen : lenbuf;
+
+            }
+
+
+            if (MaxLen == 0) return;
+            int picWidth = Oldpic.Width;
+
+            int pX= int.Parse(tbxX.Text);
+            int pY= int.Parse(tbxY.Text);
+            float sizePerWidth = float.Parse(tbxSize.Text);
+            Point StartPointBuf = new Point(pX, pY);
+            double showWidth = ((picWidth - StartPointBuf.X) * sizePerWidth);//显示一半
+            float fontSizePix = (float)(showWidth / (MaxLen + 1));//尾部流出半字的空间
+
+
+            float halfWordWidth = fontSizePix;
+            float WordWidth = halfWordWidth * 2;
+
+
+
+            using (Font f = new Font("黑体", WordWidth, FontStyle.Bold, GraphicsUnit.Pixel))
+
+            {
+                Graphics g = Graphics.FromImage(Oldpic);
+                g.DrawImage(Oldpic, 0, 0, Oldpic.Width, Oldpic.Height);
+                float idx = 0;
+                int idy = 0;
+                foreach (var stringbuf in rtbxStringBuf.Lines)
+                {
+                    idx = 0;
+                    char[] xx = stringbuf.ToCharArray();
+                    string xxstr = xx.ToString();
+                    foreach (char wordBuf in xx)
+                    {
+                        int ret = 0;
+                        float idxbuf = idx;
+                        if (wordBuf > 0xff)//汉字
+                        {
+                            SizeF sbuf = new SizeF(WordWidth * 1.0f, WordWidth);
+                            
+                            ret = GetPicAvgRGBAtPoint(Oldpic,
+                                new Point(StartPointBuf.X + (int)Math.Round(halfWordWidth * 0.3f) + (int)(idx * halfWordWidth),
+                                    StartPointBuf.Y + (int)(idy * WordWidth)), sbuf);
+                                    
+                            idxbuf += 2f;
+                        }
+                        else
+                        {
+                            SizeF sbuf = new SizeF(halfWordWidth * 1.0f, WordWidth);
+                            
+                            ret = GetPicAvgRGBAtPoint(Oldpic,
+                                new Point(StartPointBuf.X + (int)Math.Round(halfWordWidth * 0.3f) + (int)(idx * halfWordWidth),
+                                    StartPointBuf.Y + (int)(idy * WordWidth)), sbuf);
+                                    
+                            idxbuf += 1.0f;
+                        }
+                        Brush b;
+                        if (ret == 0)//黑色背景
+                        {
+                            b = new SolidBrush(Color.White);
+
+                        }
+                        else
+                        {
+                            b = new SolidBrush(Color.Black);
+
+                        }
+
+                        string addText = wordBuf.ToString();
+                        g.DrawString(addText, f, b, StartPointBuf.X + (int)(idx * halfWordWidth),
+                            StartPointBuf.Y + (int)(idy * WordWidth));
+
+
+                        idx = idxbuf;
+
+
+                    }
+
+
+
+
+                    idy++;
+                }
+
+
+                string strbufx = DateTime.Now.Ticks.ToString();
+
+
+                Oldpic.Save(("./Images/New" + strbufx + ".jpg"), System.Drawing.Imaging.ImageFormat.Jpeg);
+                NewPic = Image.FromFile("./Images/New" + strbufx + ".jpg");
+
+                pbxShowPic.BackgroundImage = NewPic;
+            }
+
+        }
+
+
+
+        int colorbuf = 200;
+        int cuowei = 0;
+        //返回值：0，黑背景，1：白背景
+        int GetPicAvgRGBAtPoint(Image ibuf, Point stratPoint, SizeF Size)
+        {
+            colorbuf = (colorbuf == 200) ? 180 : 200;
+            //cuowei = (cuowei==0) ? 5:0;
+            Bitmap newmap = new Bitmap(ibuf);
+            Color pixel;//颜色匹对
+            int width = ibuf.Width;//获取图片宽度
+            int height = ibuf.Height;//获取图片高度
+            double brightnessSum = 0;
+            int brightnessCnt = 0;
+            int andianCnt = 0;
+            int mingdianCnt = 0;
+            for (int i = stratPoint.X; i < stratPoint.X + Size.Width; )
+            {
+                for (int j = stratPoint.Y + cuowei; j < stratPoint.Y + cuowei + Size.Height; )
+                {
+                    if (i + 1 > width || j + 1 > height)
+                        break;
+                    pixel = newmap.GetPixel(i, j); //获取旧图片的颜色值（ARGB存储方式）  
+
+                    //Color nc = Color.FromArgb(45, 45, 66);
+                    //pixel = nc;
+
+                    //float bb =Math.Max(pixel.R, Math.Max(pixel.G, pixel.B));
+                    //float picHSB_B = (float)(bb/255.0); //这个是HSB/HSV 白色的程度
+
+                    //float Graybuf = (float)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+
+                    float Graybuf = 1 - (float)((0.30 * pixel.R + 0.59 * pixel.G + 0.11 * pixel.B) / 255f);
+
+                    //newmap.SetPixel(i, j, Color.FromArgb(colorbuf, colorbuf, colorbuf));
+                    //float b = pixel.GetBrightness(); //这个是HSL  亮色的程度  
+                    //Console.WriteLine(picHSB_B.ToString() + "-");
+
+                    //bri 
+                    brightnessSum += Graybuf;  //HSB中的B
+
+                    brightnessCnt++;
+
+                    if (Graybuf > (0.5))
+                    {
+                        andianCnt++;
+                    }
+                    else
+                    {
+                        mingdianCnt++;
+                    }
+                    j += 1;
+                }
+                i += 1;
+            }
+
+            //oimage = newmap;
+#if false
+
+            //采用Count
+            float andianPerAll = ((float)(andianCnt) )/ (andianCnt + mingdianCnt);
+
+            Console.WriteLine(string.Format("andianCnt:{0:F2}-mingdianCnt:{0:F2}-andianPerAll:{0:F2}-", andianCnt, mingdianCnt, andianPerAll));
+
+            if (andianPerAll > (0.7))
+            {
+              
+                return 0; //背景为黑
+            }
+            else
+            {
+                return 1; //背景为白
+            }
+
+#else
+
+            //采用平均
+            //灰度值 0.0为白，1.0为黑
+            double avggray = brightnessSum / brightnessCnt;
+
+            //Console.WriteLine(string.Format("{0:F2}-", avggray));
+
+            if (avggray > (0.5))
+            {
+
+                return 0; //背景为黑
+            }
+            else
+            {
+                return 1; //背景为白
+            }
+
+
+#endif
+        }
+
+
+      
+        public float[] hsb2rgb(float[] hsb)
+        {
+            float[] rgb = new float[3];
+            //先令饱和度和亮度为100%，调节色相h
+            for (int offset = 240, i = 0; i < 3; i++, offset -= 120)
+            {
+                //算出色相h的值和三个区域中心点(即0°，120°和240°)相差多少，然后根据坐标图按分段函数算出rgb。但因为色环展开后，红色区域的中心点是0°同时也是360°，不好算，索性将三个区域的中心点都向右平移到240°再计算比较方便
+                float x = Math.Abs((hsb[0] + offset) % 360 - 240);
+                //如果相差小于60°则为255
+                if (x <= 60) rgb[i] = 255;
+                //如果相差在60°和120°之间，
+                else if (60 < x && x < 120) rgb[i] = ((1 - (x - 60) / 60) * 255);
+                //如果相差大于120°则为0
+                else rgb[i] = 0;
+            }
+            //在调节饱和度s
+            for (int i = 0; i < 3; i++)
+                rgb[i] += (255 - rgb[i]) * (1 - hsb[1]);
+            //最后调节亮度b
+            for (int i = 0; i < 3; i++)
+                rgb[i] *= hsb[2];
+            return rgb;
+        }
+
+        public float[] rgb2hsb(float[] rgb)
+        {
+            float[] hsb = new float[3];
+            float[] rearranged = (float[])rgb.Clone();
+            int maxIndex = 0, minIndex = 0;
+            float tmp;
+            //将rgb的值从小到大排列，存在rearranged数组里
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2 - i; j++)
+                    if (rearranged[j] > rearranged[j + 1])
+                    {
+                        tmp = rearranged[j + 1];
+                        rearranged[j + 1] = rearranged[j];
+                        rearranged[j] = tmp;
+                    }
+            }
+            //rgb的下标分别为0、1、2，maxIndex和minIndex用于存储rgb中最大最小值的下标
+            for (int i = 0; i < 3; i++)
+            {
+                if (rearranged[0] == rgb[i]) minIndex = i;
+                if (rearranged[2] == rgb[i]) maxIndex = i;
+            }
+            //算出亮度
+            hsb[2] = rearranged[2] / 255.0f;
+            //算出饱和度
+            hsb[1] = 1 - rearranged[0] / rearranged[2];
+            //算出色相
+            hsb[0] = maxIndex * 120 + 60 * (rearranged[1] / hsb[1] / rearranged[2] + (1 - 1 / hsb[1])) * ((maxIndex - minIndex + 3) % 3 == 1 ? 1 : -1);
+            //防止色相为负值
+            hsb[0] = (hsb[0] + 360) % 360;
+            return hsb;
+        }
+        private void rtbxStringBuf_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
